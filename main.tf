d12b90df6b395ce16b90603bd8b9dad72061f0a5
joshuamkite <joshuamkite@gmail.com>
2022-01-17 17:24:25 +0000

    interpolate provider default tags

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "tags" {
+  type = map(string)
+  default = {
+    Owner   = "Joshua"
+    Project = "aws_acm_certificate_joshuamkite.com"
+    repo    = "git@github.com:joshuamkite/aws_acm_certificate_joshuamkite.com.git"
+  }
+}
 
 resource "aws_acm_certificate" "joshuamkite_com" {
   domain_name       = join(".", ["*", var.domain_name])
--- versions.tf
+++ versions.tf
@@ -7,14 +7,9 @@ terraform {
   }
 }
 
-
 provider "aws" {
   region = var.region
   default_tags {
-    tags = {
-      Owner   = "Joshua"
-      Project = "aws_acm_certificate_joshuamkite.com"
-      repo    = "git@github.com:joshuamkite/aws_acm_certificate_joshuamkite.com.git"
-    }
+    tags = var.tags
   }
 }
